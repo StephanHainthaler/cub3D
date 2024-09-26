55d059e68cd69747881a7ab68573a6124be6ff17
Stephan Hainthaler <shaintha@c2r4p4.42vienna.com>
2024-09-26 09:43:09 +0200

    Added MinilibX header and key definitions to cub3D.h.

--- headers/cub3D.h
+++ headers/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: shaintha <shaintha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 09:19:21 by shaintha          #+#    #+#             */
-/*   Updated: 2024/09/26 09:29:07 by shaintha         ###   ########.fr       */
+/*   Updated: 2024/09/26 09:41:37 by shaintha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,23 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <stdbool.h>
+# include <limits.h>
 # include <string.h>
 # include <fcntl.h>
 # include <sys/time.h>
+# include <mlx.h>
+# include <X11/X.h>
+# include <X11/keysym.h>
+
+//Define keys
+# define KEY_W		119
+# define KEY_S		115
+# define KEY_A		97
+# define KEY_D		100
+# define KEY_ESC	65307
+# define KEY_UP		65362
+# define KEY_DOWN	65364
+# define KEY_LEFT	65361
+# define KEY_RIGHT	65363
 
 #endif
