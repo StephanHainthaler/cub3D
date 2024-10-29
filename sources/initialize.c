ecb36e21cbe667b996a98736ad4dc9fb3f32ed49
Jayu <jayuze@gmail.com>
2024-10-29 13:58:27 +0100

    bla

--- sources/initialize.c
+++ sources/initialize.c
@@ -6,7 +6,7 @@
 /*   By: juitz <juitz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 10:35:19 by shaintha          #+#    #+#             */
-/*   Updated: 2024/10/29 12:05:05 by juitz            ###   ########.fr       */
+/*   Updated: 2024/10/29 13:45:50 by juitz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,8 +67,8 @@ void	free_images(t_cube *cube)
 
 void	init_player(t_cube *cube, t_player *player)
 {
-	size_t y;
-	size_t x;
+	size_t	y;
+	size_t	x;
 	
 	y = 0;
 	while (cube->map[y])
@@ -84,5 +84,22 @@ void	init_player(t_cube *cube, t_player *player)
 			x++;
 		}
 		y++;
+	}
+}
+
+void	init_plane(t_cube *cube, t_player *player)
+{
+	size_t	y;
+	size_t	x;
+	
+	if (cube->map[y][x] == 'N' || cube->map[y][x] == 'S')
+	{
+		player->plane_x = 0.66;
+		player->plane_y = 0;
+	}
+	if (cube->map[y][x] == 'E' || cube->map[y][x] == 'W')
+	{
+		player->plane_x = 0;
+		player->plane_y = 0.66;
 	}
 }
