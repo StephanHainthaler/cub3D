c5eacfe95b75a207a6c41dddb68d6d372fd998b0
Stephan Hainthaler <shaintha@c2r4p4.42vienna.com>
2024-11-11 13:06:21 +0100

    Added ft_strdup_until in ft_strdup.c and get_file_content.c

---
+++ libft/sources/get_file_content.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_file_content.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shaintha <shaintha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 12:34:03 by shaintha          #+#    #+#             */
+/*   Updated: 2024/11/11 12:39:03 by shaintha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/libft.h"
+
+static void put_error(char *error_message)
+{
+	ft_putendl_fd("Error", 2);
+	ft_putendl_fd(error_message, 2);
+}
+
+static char *read_file(int fd, char *line, char *temp, int bytes_read)
+{
+	int	i;
+
+	i = 0;
+	while (bytes_read > 0 && !(ft_strchr(line, EOF)))
+	{
+		bytes_read = read(fd, temp, 1);
+		if (bytes_read == -1)
+		{
+			if (line)
+				free(line);
+			line = NULL;
+			return (free(temp), put_error("Read error!"), NULL);
+		}
+		if (i == 0)
+		{
+			if (bytes_read == 0)
+				return (free(temp), free(line), put_error("Empty file!"), NULL);
+			i = 1;
+		}
+		temp[bytes_read] = '\0';
+		line = ft_strjoin_gnl(line, temp);
+		if (!line)
+			return (put_error("Malloc error!"), NULL);
+	}
+	return (free(temp), line);
+}
+
+char	*get_file_content(char *file_name)
+{
+	int		fd;
+	char	*line;
+	char	*temp;
+	int		bytes_read;
+	char	*content;
+
+	fd = open(file_name, O_RDONLY);
+	if (fd < 0)
+		return (put_error("Failed to find/open the file!"), NULL);
+	line = ft_strdup("");
+	if (line == NULL)
+		return (close(fd), put_error("Malloc error!"), NULL);
+	temp = (char *)malloc((1 + 1) * sizeof(char));
+	if (temp == NULL)
+		return (close(fd), free(line), put_error("Malloc error!"), NULL);
+	bytes_read = 1;
+	content = read_file(fd, line, temp, bytes_read);
+	if (content == NULL)
+		return (close(fd), NULL);
+	return (close(fd), content);
+}
